45cfa68e75409c43e67607c7c2b3de76d5557f43
Sr_Yuu <oie.eu.sou.um@gmail.com>
2023-02-05 01:10:51 -0300

    Add support for commands

--- functions/chat.py
+++ functions/chat.py
@@ -1,10 +1,10 @@
 from database.utils import load_db
 from database.types import GeneralUser
-from wrappers import wrappers_map
+from wrappers import wrappers_map, Wrapper
 from json import load as load_jsonfile
 from functions.state import StateProcess
 from telebot import TeleBot
-from json import dumps
+import openai
 
 class ChatBot:
     def __init__(self, messages_infos:dict, platform:str, enviroment_vars:dict, telegram_bot:TeleBot) -> None:
@@ -12,9 +12,13 @@ class ChatBot:
         self.messages_infos = messages_infos
         self.platform = wrappers_map.get(platform)
         self.retrieve_invoker(platform)
+        self.platform(self.env_vars).mark_as_read(self.invoker.user_id, self.messages_infos["messages"][0]["id"])
         self.messages_tree = load_jsonfile(open(self.env_vars.get("MESSAGES_TREE_PATH"), "r", encoding="utf-8"))
         self.telegram_bot = telegram_bot
-        self.execute_state()
+        if self.messages_infos['messages'][0]['text']['body'].startswith(self.env_vars.get("COMMAND_PREFIX")):
+            self.execute_command()
+        else:
+            self.execute_state()
 
     def retrieve_invoker(self, platform:str):
         self.invoker = GeneralUser()
@@ -39,3 +43,48 @@ class ChatBot:
     
     def execute_state(self):
         state_obj = StateProcess(self.invoker, self.messages_tree, self.env_vars, load_db(self.env_vars), self.platform, self.messages_infos, self.telegram_bot)
+    
+    def ask_chatgpt(self):
+        openai.api_key = self.env_vars.get("OPENAI_API_KEY")
+        prompt = " ".join(self.args)
+        completion = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=0.9, max_tokens=4000, n=1, stop=None)
+        self.platform = self.platform(self.env_vars)
+        self.platform.send_message(self.invoker.user_id, completion.choices[0].text)
+    
+    def askimg_chatgpt(self):
+        openai.api_key = self.env_vars.get("OPENAI_API_KEY")
+        if 'x' in self.args[0]:
+            size = self.args.pop(0)
+        else:
+            size = '1024x1024'
+        prompt = " ".join(self.args)
+        try:
+            image = openai.Image.create(prompt=prompt, n=1, size=size)
+        except:
+            pass
+        else:
+            self.platform = self.platform(self.env_vars)
+            self.platform.send_file(self.invoker.user_id, image["data"][0]["url"], "image")
+    
+    def command_doesnt_exists(self):
+        messages_tree = load_jsonfile(open(self.env_vars.get("MESSAGES_TREE_PATH"), encoding="utf-8"))
+        message_text = messages_tree["message_format"]["command_doesnt_exists"]["content"]
+        message_text = message_text.replace("|command|", self.command)
+        self.platform(self.env_vars).send_message(self.invoker.user_id, message_text)
+
+    def return_rotina(self):
+        messages_tree = load_jsonfile(open(self.env_vars.get("MESSAGES_TREE_PATH"), encoding="utf-8"))
+        message_text = messages_tree["message_format"]["rotina"]["content"]
+        self.platform(self.env_vars).send_message(self.invoker.user_id, message_text)
+
+    def execute_command(self):
+        self.args = self.messages_infos['messages'][0]['text']['body'].split()
+        self.command = self.args.pop(0)[len(self.env_vars.get("COMMAND_PREFIX")):]
+        if self.command == 'ask':
+            self.ask_chatgpt()
+        elif self.command == 'askimg':
+            self.askimg_chatgpt()
+        elif self.command == 'rotina':
+            self.return_rotina()
+        else:
+            self.command_doesnt_exists()
