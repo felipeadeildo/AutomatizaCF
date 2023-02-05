454912ee34e6609177caddf9ab1bea53623084ad
Sr_Yuu <oie.eu.sou.um@gmail.com>
2023-02-04 23:51:00 -0300

    Add TasksListener Class to Workers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from threading import Thread
 from telebot import TeleBot
 from managers.tg import TelegramListener
 from managers.api import APIListener
+from managers.tasks import TaskListener
 
 class Main:
     def __init__(self) -> None:
@@ -15,6 +16,9 @@ class Main:
         thread.start()
         
         thread = Thread(target=APIListener, args=(self.enviroment, self.telegram_bot))
+        thread.start()
+        
+        thread = Thread(target=TaskListener, args=(self.enviroment, ))
         thread.start()
 
 if __name__ == '__main__':
